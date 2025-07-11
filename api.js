23afdf97cfea6fa0b476b3d8e59f02790bf66f6d
Xavier Gomez <xavierggomey@gmail.com>
2025-07-11 12:21:11 -0400

    Update.1.0:Support for phone and showing errors visually

--- api.js
+++ api.js
@@ -1,22 +1,33 @@
+//importaciones de variables
 const container_card = document.querySelector(".container_card")
 const templates = document.getElementById("container")
+const errorBox = document.querySelector(".error_style")
+//Llamado a la api 
 async function requestAPI(pokemonId) {
   try {
+    //Pasando la respuesta a json y recuperandola en data
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
     const data = await response.json();
       return data
-
-  }catch (error) {
+    }  
+    catch (error) {
+    //Por si sucede algun error
     console.error("Error fetching Pokemon:", error);
+    errorBox.innerHTML = `<h2 style="color:#d34b4b">Error displaying Pokémon</h2>
+                          <p>Try refreshing the page</p>`
     return null;
   }
 }
+//para que cargue todo el contenido
 document.addEventListener("DOMContentLoaded",()=>{
   show_pokemon();
 })
+//Muestra 151 pokemons en el contenerdor del mismo
+//por alguna razon si no hago esa verificacion de que si tiene 1 o 2 tipos el pokemon
+//solo muestra los que tienen dos tipos
 function show_pokemon(){
+  errorBox.innerHTML ="";
 templates.innerHTML = ""
-
 for(let i = 1;i<=151;i++){
 requestAPI(i).then(data =>{
   let have = verify(data.types)
@@ -41,8 +52,20 @@ requestAPI(i).then(data =>{
   }
 }
 )
+//mostrando mas errores
+.catch(error =>{
+    console.error("Error fetching Pokemon:", error);
+    errorBox.innerHTML = `<h2 style="color:#d34b4b">Error displaying Pokémon</h2>
+                          <p>Try refreshing the page</p>`
+    return null;
+  })
 }}
+//funcion para buscar pokemon
+//cabe decir que intente que los buscara en tiempo real osea 
+//aunque el nombre no este completo por lo menos ya te este
+//apareciendo el pokemon,pero no entendi como hacerlo
 function find(id){
+  errorBox.innerHTML ="";
   id = String(id).toLowerCase();
   requestAPI(id).then(data =>{
     templates.innerHTML = 
@@ -53,7 +76,14 @@ function find(id){
     </div>`
   }
 )
+.catch(error =>{
+    console.error("Error fetching Pokemon:", error);
+    errorBox.innerHTML = `<h2 style="color:#d34b4b">Error finding Pokémon</h2>
+                          <p>Try spelling the name correctly</p>`
+    return null;
+  })
 }
+//Esta funcion verifica si el pokemon tiene dos o 1 tipo
 function verify(arr){
   let count = 0;
   for (const idx in arr) {
@@ -66,7 +96,7 @@ function verify(arr){
     return false
   }
 }
-export { find,show_pokemon};
+export {find,show_pokemon};
 
 
 
--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
+//importaciones de apis y elementos del DOM
 import {find,show_pokemon} from "./api.js"
 const text = document.getElementById("input_text")
 const input_search = document.getElementById("input_search")
 input_search.addEventListener("click",findPokemon)
+//si no hay nada en el inputText muestra los pokemons
 text.addEventListener("input",()=>
     {
         const input_text = text.value;
@@ -9,6 +11,7 @@ text.addEventListener("input",()=>
             show_pokemon();
         }
     })
+//busca el pokemon
 function findPokemon(){
     const input_text = text.value;
     find(input_text);
